5b50725206c917e5678da43c07da3ea63119aa9f
Integration-Wrathur <3397874105@qq.com>
2026-02-28 21:45:16 +0800

    成功启动所有服务

--- course-service/src/main/java/com/wrathur/course/CourseApplication.java
+++ course-service/src/main/java/com/wrathur/course/CourseApplication.java
@@ -2,10 +2,12 @@ package com.wrathur.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class, args);
--- statistic-service/src/main/java/com/wrathur/statistic/StatisticApplication.java
+++ statistic-service/src/main/java/com/wrathur/statistic/StatisticApplication.java
@@ -2,10 +2,12 @@ package com.wrathur.statistic;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StatisticApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatisticApplication.class, args);
